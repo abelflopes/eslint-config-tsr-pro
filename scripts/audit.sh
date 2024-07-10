c7967c4c6b989e2500ce5ba6d8c081ee667b11b3
Abel Lopes <abelflopes@gmail.com>
2024-07-10 19:37:21 +0100

    chore: add audit script

---
+++ scripts/audit.sh
@@ -0,0 +1,36 @@
+# Define the green color code
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo "\n"
+echo "----------------------------------------------------------------------------------------"
+echo "${GREEN}Running audit on: $(pwd)${NC}"
+echo "----------------------------------------------------------------------------------------"
+echo "\n"
+
+echo "${YELLOW}Check dependencies${NC}"
+echo "\n"
+npx --yes depcheck
+echo "\n"
+
+echo "${YELLOW}List TODO's${NC}"
+npx --yes leasot '{.,*}**' -i '**/node_modules,**/dist,**/packages' -S -x
+echo "\n"
+
+echo "${YELLOW}Check Local dependencies${NC}"
+echo "\n"
+npx --yes npm-check-updates --target latest 
+echo "\n"
+
+if [ -f "./tsconfig.json" ]; then
+    echo "${YELLOW}Check unused with ts-prune${NC}"
+    echo "\n"
+    npx --yes ts-prune
+    echo "\n"
+    
+    echo "${YELLOW}Check unused with ts-unused-exports${NC}"
+    echo "\n"
+    npx --yes ts-unused-exports tsconfig.json --showLineNumber --findCompletelyUnusedFiles --maxIssues=100
+    echo "\n"
+fi
